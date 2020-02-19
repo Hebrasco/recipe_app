e862bccb7c14874d2da0e94bc86661dc97c41fcc
Daniel Bedrich <hebrasco@icloud.com>
2020-02-19 09:46:09 +0100

    Changed lunch view layout

--- Rezepte/Views/Recipes/Categories/LunchView.swift
+++ Rezepte/Views/Recipes/Categories/LunchView.swift
@@ -9,29 +9,30 @@
 import SwiftUI
 
 struct LunchView: View {
+    let testRecipe = Recipe(image: "placeholder", title: "Bananenaufstrich", ingredients: [], intolerances: ["halal", "wheat"], category: "Frühstück", tags: "Erster, Zweiter, Dritter Tag", time: 30, difficulty: .medium)
+    
     var body: some View {
-        ScrollView {
-            VStack(alignment: .center, spacing: 10) {
-                Category(name: "Fleisch",
-                          image: "meat",
-                          destination: AnyView(MeatView()))
-                Category(name: "Fish",
-                          image: "fish",
-                          destination: AnyView(FishView()))
-                Category(name: "Vegetarisch",
-                          image: "vegetarian",
-                          destination: AnyView(VegetarianView()))
-                Category(name: "Pasta & Co",
-                          image: "pasta",
-                          destination: AnyView(PastaView()))
-                Category(name: "Süßes",
-                          image: "sweets",
-                          destination: AnyView(SweetsView()))
-                Category(name: "Suppen",
-                          image: "soup",
-                          destination: AnyView(SoupView()))
-            }
-            .frame(width: UIScreen.main.bounds.width)
+        List {
+            Category(name: "Fleisch",
+                      image: "meat",
+                      destination: AnyView(MeatView()))
+            Category(name: "Fish",
+                      image: "fish",
+                      destination: AnyView(FishView()))
+            Category(name: "Vegetarisch",
+                      image: "vegetarian",
+                      destination: AnyView(VegetarianView()))
+            Category(name: "Pasta & Co",
+                      image: "pasta",
+                      destination: AnyView(PastaView()))
+            Category(name: "Süßes",
+                      image: "sweets",
+                      destination: AnyView(SweetsView()))
+            Category(name: "Suppen",
+                      image: "soup",
+                      destination: AnyView(SoupView()))
+            RecipeCard(recipe: testRecipe)
+            RecipeCard(recipe: testRecipe)
         }
         .navigationBarTitle("Mittagessen", displayMode: .inline)
     }
