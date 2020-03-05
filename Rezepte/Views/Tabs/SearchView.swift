5c8cc791771688363460fdb488329b1191fd51be
Daniel Bedrich <hebrasco@icloud.com>
2020-03-05 13:39:18 +0100

    Added search view

--- Rezepte/Views/Tabs/SearchView.swift
+++ Rezepte/Views/Tabs/SearchView.swift
@@ -8,11 +8,48 @@
 
 import SwiftUI
 
+extension UIApplication {
+    func endEditing(_ force: Bool) {
+        self.windows
+            .filter{$0.isKeyWindow}
+            .first?
+            .endEditing(force)
+    }
+}
+
+struct ResignKeyboardOnDragGesture: ViewModifier {
+    var gesture = DragGesture().onChanged { _ in
+        UIApplication.shared.endEditing(true)
+    }
+    func body(content: Content) -> some View {
+        return content.gesture(gesture)
+    }
+}
+
+extension View {
+    func resignKeyboardOnDragGesture() -> some View {
+        return modifier(ResignKeyboardOnDragGesture())
+    }
+}
+
 struct SearchView: View {
     @ObservedObject var viewModel = SearchViewModel()
+    let recipes = Recipes.recipes
+    @State var showCancelButton = false
     
     var body: some View {
-        Text("Suche")
+        NavigationView {
+            VStack {
+                SearchBar(searchText: $viewModel.searchText)
+                List {
+                    ForEach(recipes.indices, id: \.self) { index in
+                        RecipeCard(recipe: self.recipes[index])
+                    }
+                }
+            }
+            .resignKeyboardOnDragGesture()
+            .navigationBarTitle("Search")
+        }
     }
 }
 
