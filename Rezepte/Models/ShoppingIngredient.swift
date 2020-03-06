4f2b2e8291b95c0a5cbe687bc1aaf2c0535155e0
Daniel Bedrich <hebrasco@icloud.com>
2020-03-06 10:00:36 +0100

    Added shopping list view

---
+++ Rezepte/Models/ShoppingIngredient.swift
@@ -0,0 +1,16 @@
+//
+//  ShoppingIngredient.swift
+//  Rezepte
+//
+//  Created by Daniel Bedrich on 05.03.20.
+//  Copyright © 2020 Daniel Bedrich. All rights reserved.
+//
+
+import Foundation
+
+struct ShoppingIngredient {
+    let name: String
+    let amount: String
+    let unit: String
+    var isChecked = false
+}
