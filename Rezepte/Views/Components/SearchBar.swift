869976badd9bbe3c8501dde23a347df749ffb3ec
Daniel Bedrich <hebrasco@icloud.com>
2020-03-06 12:51:21 +0100

    Refactored some code

--- Rezepte/Views/Components/SearchBar.swift
+++ Rezepte/Views/Components/SearchBar.swift
@@ -8,6 +8,30 @@
 
 import SwiftUI
 
+extension UIApplication {
+    func endEditing(_ force: Bool) {
+        self.windows
+            .filter{$0.isKeyWindow}
+            .first?
+            .endEditing(force)
+    }
+}
+
+struct ResignKeyboardOnDragGesture: ViewModifier {
+    var gesture = DragGesture().onChanged { _ in
+        UIApplication.shared.endEditing(true)
+    }
+    func body(content: Content) -> some View {
+        return content.gesture(gesture)
+    }
+}
+
+extension View {
+    func resignKeyboardOnDragGesture() -> some View {
+        return modifier(ResignKeyboardOnDragGesture())
+    }
+}
+
 struct SearchBar: View {
     @State var showCancelButton = false
     @Binding var searchText: String
